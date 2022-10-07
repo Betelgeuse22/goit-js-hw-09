a55c65f2d5d94243d6ac45f548d2f7d6292d1634
Betelgeuse22 <104512953+Betelgeuse22@users.noreply.github.com>
2022-10-07 21:34:32 +0000

    Deploying to gh-pages from @ Betelgeuse22/goit-js-hw-09@a7a349cef24160552ef932ff501ad83c8111060c 🚀

---
+++ 01-color-switcher.209747fc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){d=setInterval((function(){t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.209747fc.js.map
